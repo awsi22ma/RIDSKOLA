caf085d78d9731d55891976d48e5bdc5d3399c41
silva <silva.88s@live.com>
2022-10-03 23:39:50 +0200

    lägg till

---
+++ script.js
@@ -0,0 +1,6 @@
+const sms=document.getElementById("meddela");
+const smsbtn=document.getElementById("meddelabtn");
+
+meddelabtn.addEventListener("click",function()
+{console.log("click på knapp");
+console.log(meddela.value)})
